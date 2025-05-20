58b40756be0686cba29cb5622737fa63a24be349
Picsart Academy <picsartacademy@Alvards-MacBook-Pro-5.local>
2025-05-20 21:17:31 +0400

    structural changes

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# app.include_router(example.router)
-
 @app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+def root():
+    return {"message": "Hello from FastAPI"}
--- main2.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append("QueryProcessor")
-from QueryProcessor.server import Engine
-
-if __name__ == "__main__":
-    engine = Engine()
-    engine.start()
